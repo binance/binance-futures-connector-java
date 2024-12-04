e6acb6f60860d8a95cd8ba12f76f1b48eaa4182e
Anderson Souza <andersonpds14@gmail.com>
2024-12-03 23:09:28 -0300

    feat: add Account information v3 example

---
+++ src/test/java/examples/um_futures/account/AccountInformationV3.java
@@ -0,0 +1,32 @@
+package examples.um_futures.account;
+
+import com.binance.connector.futures.client.exceptions.BinanceClientException;
+import com.binance.connector.futures.client.exceptions.BinanceConnectorException;
+import com.binance.connector.futures.client.impl.UMFuturesClientImpl;
+import examples.PrivateConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedHashMap;
+
+public final class AccountInformationV3 {
+    private AccountInformationV3() {
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountInformationV3.class);
+    public static void main(String[] args) {
+        LinkedHashMap<String, Object> parameters = new LinkedHashMap<>();
+
+        UMFuturesClientImpl client = new UMFuturesClientImpl(PrivateConfig.TESTNET_API_KEY, PrivateConfig.TESTNET_SECRET_KEY, PrivateConfig.TESTNET_BASE_URL);
+
+        try {
+            String result = client.account().accountInformationV3(parameters);
+            logger.info(result);
+        } catch (BinanceConnectorException e) {
+            logger.error("fullErrMessage: {}", e.getMessage(), e);
+        } catch (BinanceClientException e) {
+            logger.error("fullErrMessage: {} \nerrMessage: {} \nerrCode: {} \nHTTPStatusCode: {}",
+                    e.getMessage(), e.getErrMsg(), e.getErrorCode(), e.getHttpStatusCode(), e);
+        }
+    }
+}
